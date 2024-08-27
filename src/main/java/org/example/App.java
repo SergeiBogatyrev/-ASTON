de25a3d08e06926354193ea379eda3245802b53d
SergeiBogatyrev <154222336+SergeiBogatyrev@users.noreply.github.com>
2024-08-27 19:15:25 +0300

    Add files via upload

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        FactorialJunit5 a = new FactorialJunit5();
+        int n = 5;
+        long i = a.getFactorial(n);
+        System.out.println("Факториал " + n + " равняется " + i);
+
+    }
+}
---
+++ src/test/java/org/example/FactorialJunit5Test.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FactorialJunit5Test {
+
+    @Test
+    void getFactorial() {
+        FactorialJunit5 a = new FactorialJunit5();
+        long actual = a.getFactorial(5);
+        long expected = 120;
+        assertEquals(expected, actual);
+    }
+}
